ad4c4f062262d66a6e120608ddb61103e7db0d4b
Alexandrei <alexandreirdp@gmail.com>
2017-11-19 12:52:44 -0400

    debugged and fixed most stuff, finished

--- src/assets/main.js
+++ src/assets/main.js
@@ -8,82 +8,90 @@ let replayDiv = document.getElementById("replay-div");
 
 function guess() {
   let input = document.getElementById('user-guess');
+  if ((answer.value.length === 0) || (attempt === 0)) {
+    setHiddenFields();
+  }
   //add functionality to guess function here
   if (!validateInput(input.value)) {
     return;
   } else {
     attempt += 1;
   }
-
-  if (!getResults(input.value) && attempt >= 10) {
+  var result = getResults(input.value);
+  if (!result && attempt >= 10) {
     setMessage("You Lose! :(");
     showAnswer(false);
     showReplay();
-  } else {
+  }  else if(!result)
+    {
+        setMessage("Incorrect, try again.");
+    }
+   else {
     setMessage("You Win! :)");
     showAnswer(true);
     showReplay();
   }
 };
 
-  //implement new functions here
+//implement new functions here
 
-  function setHiddenFields() {
-    if (attempt.toString === "") {
-      attempt = 0;
-    }
-    if (answer.toString === "") {
-      answer = Math.floor(Math.random(0, 9999));
-    }
-    while (answer.toString().length() > 4) {
-      answer += "0";
-    }
-  };
+function setHiddenFields() {
+  if (attempt.value.length === 0) {
+    attempt = 0;
+  }
+  if (answer.value.length === 0) {
+    answer.value = Math.floor(Math.random() * 10000);
+  }
+  while (answer.value.toString().length > 4) {
+    answer.value += "0";
+  }
+};
 
-  function setMessage(param) {
-    message.innerHTML = param;
-  };
+function setMessage(param) {
+  message.innerHTML = param;
+};
 
-  function validateInput(param) {
-    if (param.length === 4)
-      return true;
-    else {
-      setMessage("Guesses must be exactly 4 characters long.");
-      return false;
-    }
-  };
+function validateInput(param) {
+  if (param.length === 4)
+    return true;
+  else {
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  }
+};
 
-  function getResults(param) {
-    var win = false;
-    var text = '<div class="row"><span class="col-md-6">' + param + '</span><div class="col-md-6">';
-    for (var i = 0; i <= param.length; i++) {
-      if (param.charAt(i) === answer.charAt(i)) {
-        text += '<span class = "glyphicon glyphicon-ok" > </span>';
-      } else if param.charat(i).indexOf(answer) {
-        '<span class = "glyphicon glyphicon-transfer"> </span>';
-      } else {
-        '<span class = "glyphicon glyphicon-remove" > </span>';
-      }
-    }
-    '</div>'
-
-    if (param === answer) {
-      return true;
+function getResults(param) {
+  var win = false;
+  var text = '<div class="row"><span class="col-md-6">' + param + '</span><div class="col-md-6">';
+  for (var i = 0; i < param.length; i++) {
+    if (param.charAt(i) === answer.value.charAt(i)) {
+      text += '<span class = "glyphicon glyphicon-ok" > </span>';
+    } else if (param.charAt(i).indexOf(answer.value) === true) {
+      text +='<span class = "glyphicon glyphicon-transfer"> </span>';
     } else {
-      return false;
+     text+=  '<span class = "glyphicon glyphicon-remove" > </span>';
     }
-  };
+  }
+  '</div>'
 
-  function showAnswer(param) {
-    code.innerHTML = answer.value;
-    if (param === true){
-      code.className = "success";
-    }else {
-      code.className = "failure";
-    }
-  };
+results.innerHTML += text;
+  if (param === answer.value) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-  function showReplay(){
-    guessingDiv.style.display = "none";
-    replayDiv.style.display = "block";
-  };
+function showAnswer(param) {
+  code.innerHTML = answer.value;
+  if (param === true) {
+    code.className = "success";
+  } else {
+    code.className = "failure";
+  }
+};
+
+function showReplay() {
+  guessingDiv.style.display = "none";
+  replayDiv.style.display = "block";
+};
